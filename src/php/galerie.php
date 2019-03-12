fc5a23b9da03d0d81444331c488e7bcad4495512
simonbourrasseau <simon.bourrasseau@imie.fr>
2019-03-12 10:22:22 +0100

    lien

---
+++ src/php/galerie.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="FR-fr">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/theme.css">
+    <title>Bulle de Fil</title>
+</head>
+<body>
+
+<a href="../../index.html">Accueil</a>
+<a href="../php/galerie.php">galerie</a>
+<a href="../php/contact.php">contact</a>
+
+</body>
+</html>
