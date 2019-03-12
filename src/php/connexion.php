728e3358bd45c5b92cff2eaadfb44b79a96c1465
Nathan Gracia <nathan.gracia.863@gmail.com>
2019-03-12 16:20:57 +0100

    up admin.php

--- src/php/connexion.php
+++ src/php/connexion.php
@@ -11,8 +11,7 @@ if (isset($_POST["formconnexion"])) {
 	$login = htmlspecialchars($_POST["login"]);
 	//$mdpconnect = sha1($_POST["mdpconnect"]);
     $mdpconnect = $_POST["mdpconnect"];
-    echo $_POST['login'];
-    echo $_POST['mdpconnect'];
+    
 	if (!empty($login) AND !empty($mdpconnect)) {
 		$requser = $bdd ->prepare("SELECT * FROM logadmin WHERE login = ? AND mdp = ?");
 		$requser->execute(array($login, $mdpconnect));
