25a24fc21158dae716a9ede44d4c654c0761adfa
Margaux <margaux.dechaud@imie.fr>
2019-03-12 15:30:34 +0100

    màj

--- src/php/footer.php
+++ src/php/footer.php
@@ -1,6 +1,6 @@
 <footer>
   <ul>
     <a href="../html/cgu.html">CGU</a>
-    <a href="../html/mention_legal.html">Mention Legal</a>
+    <a href="../html/mention_legal.html">Mentions Légales</a>
   </ul>
 </footer>
