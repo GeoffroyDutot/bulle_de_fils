531eaaa78867501c65b11e5a7fbd445426d59287
Nicolas Hamy <nhamypro@gmail.com>
2019-03-12 12:04:06 +0100

    V1 page galerie

--- src/php/galerie.php
+++ src/php/galerie.php
@@ -8,18 +8,43 @@
 <body>
 
 <header>
-
+    <img  id="logo" src="../images/logo.jpg">
     <img id="entete_galerie"src="../images/couture.jpg">
     <strong><h1 id="titre_galerie">Galerie</h1></strong>
 
 
 </header>
-<div id="bande_couleur1">salut</div>
+
 <a href="../../index.html">Accueil</a>
 <a href="../php/galerie.php">galerie</a>
 <a href="../php/contact.php">contact</a>
 
+<table id="tableau_galerie">
+    <tr>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+    </tr>
 
+    <tr>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+    </tr>
+
+    <tr>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+    </tr>
+
+    <tr>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+        <td> <img src="../images/couture.jpg" width="300px"></td>
+    </tr>
+
+</table>
 <footer>
     <a href="../html/cgu.html">CGU</a>
     <a href="../html/mention_legal.html">Mention Legal</a>
