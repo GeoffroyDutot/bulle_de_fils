70cf59c57d53f1f99e5793e9322bf1dcb9c391e0
simonbourrasseau <simon.bourrasseau@imie.fr>
2019-03-13 15:43:54 +0100

    center

--- src/php/bank.php
+++ src/php/bank.php
@@ -19,13 +19,13 @@
 <?php include('navbar.php') ?> <br>
 
 <p class="text_tissus">Plus de 150 tissus bio ou oeko tex comme par exemple du tissus uni ou étoile ou encore liberty. Tous les tissus utilisés sont bios ou oeko tex, un label écologique pour les produits textiles, garantissant l'absence de substances nocives pour la santé, la peau et l'environnement. </p>
-
+<center>
 <table class="tableau_tissus">
     <tr>
         <td><img src="../images/tissus_uni.jpg" class="imgtissu"><br><p>tissus uni</p</td><td><img src="../images/tissus%20liberty.jpg" class="imgtissu"><br><p>tissus liberty</p></td><td><img src="../images/tissus__etoile_gris.jpg" class="imgtissu"><br><p>tissus étoile</p></td>
     </tr>
 </table>
-
+</center>
 <?php include('footer.php')?>
 
 </body>
