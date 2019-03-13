2f83f3e7705afbcd7f36ba2b22bb52d7e764c2f5
simonbourrasseau <simon.bourrasseau@imie.fr>
2019-03-13 09:23:10 +0100

    bank menu

--- index.php
+++ index.php
@@ -23,7 +23,8 @@
 <div class="menu">
     <ul>
         <li><a href="./index.php">Accueil</a></li>
-        <li><a href="./src/php/galerie.php">Banque de tissus</a></li>
+        <li><a href="./src/php/galerie.php">Galerie</a></li>
+        <li><a href="./src/php/bank.php">Banque de tissus</a></li>
         <li><a href="./src/php/contact.php">Contact</a></li>
     </ul>
 </div>
