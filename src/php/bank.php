a02883dee8423cb768f57fe1500512c0eb612d05
simonbourrasseau <simon.bourrasseau@imie.fr>
2019-03-13 14:05:59 +0100

    rebank

--- src/php/bank.php
+++ src/php/bank.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
-    <link rel="stylesheet" type="text/css" href="../css/galerie.css">
+    <link rel="stylesheet" type="text/css" href="../css/bank.css">
     <link rel="stylesheet" type="text/css" href="../css/theme.css">
     <title>Bank de fil</title>
 </head>
